9706e3f49a55d535c197f7c9abeb0b983f2657d8
jlh-ipeps <jean.louis.hens.ipeps@gmail.com>
2016-12-02 08:54:52 +0100

    languages list to sidebarCtrl + assts to public

--- src/AppBundle/Controller/SidebarController.php
+++ src/AppBundle/Controller/SidebarController.php
@@ -6,32 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SidebarController extends Controller {
 
-  public function viewAction($originalRequest) {
+    public function viewAction($originalRequest) {
+      
+        $session = $this->get('session');
+        $session->set('_locale', $originalRequest->getLocale());
 
-    $session = $this->get('session');
-    $session->set('_locale', $originalRequest->getLocale());
+    //    dump($originalRequest->get('_route_params'), $originalRequest->get('_route_params')['id'], $originalRequest->get('_route'));die();
 
-//    dump($originalRequest->get('_route_params'), $originalRequest->get('_route_params')['id'], $originalRequest->get('_route'));die();
+        if ($originalRequest->get('_route') == 'book') {
+            $session->set('book', $originalRequest->get('_route_params')['id']);
+        } else {
+            $session->set('book', NULL);
+        }
 
-    if ($originalRequest->get('_route') == 'book') {
-        $session->set('book', $originalRequest->get('_route_params')['id']);
-    } else {
-        $session->set('book', NULL);
+        $em = $this->getDoctrine()->getManager();
+        $bookRepo = $em->getRepository('AppBundle:Book');
+
+        if ($this->getUser() == NULL) {
+            $mybooks = NULL;
+        } else {
+            $mybooks = $bookRepo->findBooksByUser($this->getUser()->getId());
+        }
+
+        return $this->render('AppBundle:layout:sidebar.html.twig', array(
+            "locales" => array(
+                'de' => "Deutch",
+                'en' => "English",
+                'fr' => "Français",
+                'nl' => "Nederlands"
+            ),
+            'mybooks' => $mybooks,
+            'originalRequest' => $originalRequest,
+            'session' => $session,
+        ));
     }
-    
-    $em = $this->getDoctrine()->getManager();
-    $bookRepo = $em->getRepository('AppBundle:Book');
-    
-    if ($this->getUser() == NULL) {
-        $mybooks = NULL;
-    } else {
-        $mybooks = $bookRepo->findBooksByUser($this->getUser()->getId());
-    }
-
-    return $this->render('AppBundle:layout:sidebar.html.twig', array(
-      'mybooks' => $mybooks,
-      'originalRequest' => $originalRequest,
-      'session' => $session,
-    ));
-  }
 }
