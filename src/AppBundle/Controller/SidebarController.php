8e615ed29d2781997417b125c6d1a24d94f3913e
jlh-ipeps <jean.louis.hens.ipeps@gmail.com>
2017-02-07 14:39:35 +0100

    mybooks

--- src/AppBundle/Controller/SidebarController.php
+++ src/AppBundle/Controller/SidebarController.php
@@ -29,7 +29,9 @@ class SidebarController extends Controller {
             
         if ($user) {
             $bookRepo = $em->getRepository('AppBundle:Book');
-            $mybooks = $bookRepo->findBooksByUser($this->getUser()->getId())->getQuery()->getResult();
+//            $mybooks = $bookRepo->findBooksByUser($this->getUser()->getId())->getQuery()->getResult();
+            $mybooks = $this->getUser()->getBooks();
+//            dump($mybooks2, $mybooks);die();
 
             $sessionRepo = $em->getRepository('AppBundle:LastSession');
 
