7b2dacc13122ce07576862e33fd2bf02e30f219f
jlh-ipeps <jean.louis.hens.ipeps@gmail.com>
2017-02-11 12:57:58 +0100

    access right app.php hacked

--- web/app.php
+++ web/app.php
@@ -2,6 +2,8 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+exec ("find /home/hens/www/time-view/web/thumb/time-view/web/uploads/img/ -type f -exec chmod 0644 {} +");
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
